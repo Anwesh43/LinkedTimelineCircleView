53398d0ea8cda72e2d91c26b4858ba3d468746a4
Anwesh43 <anweshthecool0@gmail.com>
2018-08-24 02:59:23 +0530

    adding TimelineCircleView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtimelinecircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtimelinecircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtimelinecircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.timelinecircleview.TimelineCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TimelineCircleView.create(this)
     }
 }
