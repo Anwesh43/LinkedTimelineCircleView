fa44aac7b7eafb29606ed97dd1b9eda348ddac10
Anwesh43 <anweshthecool0@gmail.com>
2018-08-24 02:53:57 +0530

    using renderer in view

--- timelinecircleview/src/main/java/com/anwesh/uiprojects/timelinecircleview/TimelineCircleView.kt
+++ timelinecircleview/src/main/java/com/anwesh/uiprojects/timelinecircleview/TimelineCircleView.kt
@@ -37,14 +37,18 @@ fun Canvas.drawTCNode(i : Int, scale : Float, paint : Paint) {
 
 class TimelineCircleView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
